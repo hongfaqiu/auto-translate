e5e72b232ea573dd86bc017458c12022b9f43180
hongfaqiu <951142905@qq.com>
2022-05-19 09:36:13 +0800

    DOC: update readme

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Auto-translate
 
+使用谷歌api进行自动翻译的脚本
+
 ## Usage
 
+在``loclaes``目录下新建需要翻译的字典，配置``transConfig.ts``，更改目标文件夹和需要输入的语言、文件名，执行``npm run translate``即可进行翻译
+
 ```bash
+pnpm install
 npm run translate
 ```
--- transConfig.ts
+++ transConfig.ts
@@ -1,14 +1,18 @@
 import { TranslateConfig } from "./src/typings";
 
 const config: TranslateConfig = {
-  readFolder: './locales/mapView', // 读取的目标文件夹
-  from: 'zh-CN', // 不填则默认自动识别
+  // 读取的目标文件夹，目标文件夹中只能存在一种语言的字典
+  readFolder: './locales/mapView',
+   // 不填则默认自动识别
+  from: 'zh-CN',
+   // 对照文件地址映射,不要与源文件夹一致,再次执行会覆盖同名文件
   outPutFolder: {
-    en: './build/en-US.ts',
-    pt: './build/pt-BR.ts',
-    'zh-TW': './build/zh-TW.ts'
-  }, // 对照文件地址映射,不要与源文件夹一致
-  suffix: 'ts', // 指定的文件后缀
+    en: './build/mapView/en-US.ts',
+    pt: './build/mapView/pt-BR.ts',
+    'zh-TW': './build/mapView/zh-TW.ts'
+  },
+   // 指定的文件后缀
+  suffix: 'ts',
 }
 
 export default config
